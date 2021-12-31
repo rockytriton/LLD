56b024cfbf47498bae3b9318bc0d8f0d83e3e93f
rockytriton <rockytriton@yahoo.com>
2021-12-30 21:50:34 -0600

    update mem header

--- rpi_bm/part16/include/mem.h
+++ rpi_bm/part16/include/mem.h
@@ -11,3 +11,6 @@ void *memcpy(void *dest, const void *src, u32 n);
 #define BUS_ADDRESS(addr)	(((addr) & ~0xC0000000) | GPU_MEM_BASE)
 
 void *get_free_pages(int num_pages);
+void *allocate_memory(int bytes);
+void free_memory(void *base);
+
